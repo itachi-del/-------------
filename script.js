250fcfbdf2051c1e2befe876d24171f41f28f247
esim <esekenov.e05@gmail.com>
2024-11-11 10:59:04 +0500

    емдихан

---
+++ script.js
@@ -0,0 +1,47 @@
+ let san=prompt("1-ден 12ге дейін сан енгізініз")
+switch(san){
+         case "1":
+          alert(" Ай 1,january")
+              break;
+          case"2":
+           alert("Ай 2,february")
+           break;
+           case "3":
+              alert("Ай 3,march")
+              break;
+           case"4":
+              alert("Ай 4,april")
+              break;
+           case "5":
+              alert("Ай 5,may")
+              break;
+           case"6":
+              alert("Ай 6,june")    
+              break;
+           case"7":
+              alert("Ай 7,july")
+              break;
+           case"8":
+              alert("Ай 8,august")
+              break;
+            case"9":
+              alert("Ай 9,september")
+              break;
+            case"10":
+              alert("Ай 10,octomber")
+              break;
+            case"11":
+              alert("Ай 11,november")
+              break;
+            case"12":
+              alert("Ай 12,december")
+              break;              
+           default:
+            alert("сіз қате тердіңіз қайта енгізуді өтінеміз")
+          }  
+
+
+ 
+
+ 
+
